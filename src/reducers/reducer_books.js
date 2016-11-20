8d1a1cd5347b4912a6f81bdd6aaeb1d7411814fc
Federico V Garcia <federico.v.garcia@gmail.com>
2016-11-20 09:00:47 -0500

    add redux: reducers and actions
    Signed-off-by: Federico V Garcia <federico.v.garcia@gmail.com>

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+  return [
+    {title: 'JS the good parts', pages: 123},
+    {title: 'YDKJS', pages: 531},
+    {title: 'Exploring ES6', pages: 838},
+    {title: 'React and Redux', pages: 62},
+    {title: 'Next.js', pages: 45},
+  ];
+}
